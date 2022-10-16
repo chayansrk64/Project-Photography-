99bebd4043cf3223a23e8fb4d21b8aac5e686f1d
chayansrk64 <chayansrk64@gmail.com>
2022-10-17 00:08:05 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,71 @@
+// change nav style on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scrolled',
+    scrollY > 0)
+})
+
+
+
+// contact buttons (circle text button)
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButtons => {
+    let text = textButtons.querySelector('p');
+
+    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg">${character}</span>`).join('')
+})
+
+//  swiper js--------------------
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        599: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+  });
+
+  
+
+
+   const nav = document.querySelector('.nav__links');
+   const openNavBtn = document.querySelector('#nav__toggle-open');
+   const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+
+   const opneNav = () => {
+    nav.style.display = 'flex';
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display ='inline-block';
+   }
+
+   openNavBtn.addEventListener('click', opneNav)
+
+
+   const closeNav = () => {
+    nav.style.display = 'none';
+    openNavBtn.style.display = 'inline-block';
+    closeNavBtn.style.display ='none';
+   }
+
+   closeNavBtn.addEventListener('click', closeNav);
+
+//    close nav menu on click--------------- 
+
+   if(document.body.clientWidth < 1024) {
+    nav.querySelectorAll('li a').forEach(navLink => {
+        navLink.addEventListener('click', closeNav);
+       })
+    
+   }
